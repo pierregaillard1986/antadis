81663f5c015ef14ad4cbed03e5a81372c21db472
pierregaillard1986 <pierregaillard1986@outlook.com>
2018-03-16 16:48:08 +0100

    axios and vue-resource integration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
+// integration VueResource
+import VueResource from 'vue-resource'
+
+// integration axios
+import axios from 'axios';
 // integration jquery
 import jQuery from 'jquery'
 global.jQuery = jQuery
@@ -10,6 +16,7 @@ let Bootstrap = require('bootstrap')
 require('bootstrap/dist/css/bootstrap.min.css');
 
 Vue.config.productionTip = false
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
