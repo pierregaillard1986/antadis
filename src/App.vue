970a9a6e72490fef955b9934309c75c04d14dca5
pierregaillard1986 <pierregaillard1986@outlook.com>
2018-03-20 11:53:36 +0100

    connect api via axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
     <myCard/>
+    {{posts.name}}
   </div>
 </template>
 
 <script>
 import myCard from './components/my-card'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     myCard
+  },
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+  created() {
+    axios.get(`https://cors.io/?http://gaillard_k9gko.recrutement.antadis.fr/api/product.json`)
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
   }
 }
 </script>
